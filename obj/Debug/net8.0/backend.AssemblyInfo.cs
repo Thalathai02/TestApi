25fa5c95df32af48a4a6840bd2d1c2cbca91d97c
thalathai.si <thalathai02@gmail.com>
2025-02-21 16:03:46 +0700

    up

--- obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ead0b0545249b3a45edcf182e811e56eb697420")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bac6beaa18ad958e20a063770b70e32cca1d0f4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
